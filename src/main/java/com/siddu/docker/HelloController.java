0ca2378c9d719ac1a60fca559c13fef00133d2be
siddu <siddubasha255@gmail.com>
2025-11-29 15:29:29 +0530

    welcome REST API developed

---
+++ src/main/java/com/siddu/docker/HelloController.java
@@ -0,0 +1,14 @@
+package com.siddu.docker;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	
+	@GetMapping("/")
+	public String hello() {
+		return "Hello, Welcome to Siddu IT";
+	}
+}
